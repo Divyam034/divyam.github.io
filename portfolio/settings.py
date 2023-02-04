190488f6ea5f64b259a2f1f54893705d9b716daa
Divyam <divyam2116@gmail.com>
2023-02-04 22:40:32 +0530

    changed the template file location

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -50,7 +50,9 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, ''),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
